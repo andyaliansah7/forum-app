d72590b2f2bd7f5b5a3b361a6178c1d168e49676
andyaliansah7 <andyaliansah7@gmail.com>
2025-01-27 01:24:06 +0700

    remove cdn and change it to import bootsrap

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
 import store from './states';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
